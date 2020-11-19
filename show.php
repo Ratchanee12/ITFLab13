49aa3ce03acde0a5902aa963a1faded39f6dac32
Ratchanee12 <73681315+Ratchanee12@users.noreply.github.com>
2020-11-19 20:45:50 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,6 +1,12 @@
 <html>
 <head>
-<title>ITF Lab</title>
+<title>How are you today</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php
@@ -8,36 +14,40 @@ $conn = mysqli_init();
 mysqli_real_connect($conn, '63070149itflab.mysql.database.azure.com', 'Ratchanee@63070149itflab', 'FWTsod26', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
+    die('Failed to connect to MySQL PLEASE TRY AGAIN !: '.mysqli_connect_error());
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
-    <th width="200"> <div align="center">Function </div></th>
-  </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr>
-    <td><?php echo $Result['name'];?></div></td>
-    <td><?php echo $Result['comment'];?></td>
-    <td><?php echo $Result['link'];?></td>
-    <td>
-      <a href ="delete.php?ID=<?php echo $Result['ID'];?>" >Delete</a>
-      <a href ="update.php?ID=<?php echo $Result['ID'];?>" >Update</a>
-    </td>
-  </tr>
-<?php
-}
-?>
-</table>
-<?php
-mysqli_close($conn);
-?>
+
+<div class="container">
+    <div class="form">
+        <p class="ex1"><h1><center>How are you today</center></h1></p>
+    </div>
+  <table width="800" border="1" class="table table-dark table-hover table-striped" class="center" >
+    <thead class="thead-dark">
+    <tr class="active">
+      <th width="200"> <div align="center">Name</div></th>
+      <th width="500"> <div align="center">Felt</div></th>
+      <th width="100"> <div align="center">Action</div></th>
+    </tr>
+     </thead>
+  <?php
+  while($Result = mysqli_fetch_array($res))
+  {
+  ?>
+    <tr>
+      <td><center><?php echo $Result['Name'];?></center></td>
+      <td><center><?php echo $Result['Comment'];?></center></td>
+      <td><center><a href="formindex.html"><input type="submit" value="Edit" class="btn-default"></a>&nbsp;&nbsp;<a href="formdelete.html"><input type="submit" value="Delete"  class="btn-default"></a></center></td>
+    </tr>
+  <?php
+  }
+  ?>
+  </table>
+  <a href="forminsert.html"><input type="submit" value="ADD" class="btn-default btn-sm"></a>
+  <?php
+  mysqli_close($conn);
+  ?>
+</div>
 </body>
 </html>
