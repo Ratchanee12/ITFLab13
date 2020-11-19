c69fcdf409a6531cd4c7c279bd8d912c8571e774
Ratchanee12 <73681315+Ratchanee12@users.noreply.github.com>
2020-11-19 19:26:38 +0700

    Update show.php

--- show.php
+++ show.php
@@ -5,7 +5,7 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, '63070149itflab.mysql.database.azure.com', 'Ratchanee@63070149itflab', 'FWTsod26', 'itfLab', 3306);
+mysqli_real_connect($conn, '63070149itflab.mysql.database.azure.com', 'Ratchanee@63070149itflab', 'FWTsod26', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
