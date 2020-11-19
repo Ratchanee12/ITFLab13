7369271263f9b258c8d5230d52b2381e3aaaa043
Ratchanee12 <73681315+Ratchanee12@users.noreply.github.com>
2020-11-19 20:55:23 +0700

    Update and rename delete (1).php to delete.php

---
+++ delete.php
@@ -0,0 +1,25 @@
+<?php
+
+$conn = mysqli_init();
+$conn = mysqli_connect('63070149itflab.mysql.database.azure.com', 'Ratchanee@63070149itflab', 'FWTsod26', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL PLEASE TRY AGAIN: '.mysqli_connect_error());
+}
+
+
+$name = $_POST['name'];
+$comment = $_POST['comment'];
+
+
+$sql = $sql = "DELETE FROM guestbook WHERE name='$name'";
+$sql = "DELETE FROM guestbook WHERE Comment='$comment'";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully! CONGRATULATION!";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
