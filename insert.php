3366080fe00eb765a3ef5dd0b06a5363630cd62d
Ratchanee12 <73681315+Ratchanee12@users.noreply.github.com>
2020-11-19 20:16:05 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'panwit.mysql.database.azure.com', 'panwit@panwit', '123456789', 'itflab', 3306);
+mysqli_real_connect($conn, '63070149itflab.mysql.database.azure.com', 'Ratchanee@63070149itflab', 'FWTsod26', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
